0239721b169c0a9b08084b72e67f5db0ed9976ab
unknown <devjunpark@gmail.com>
2015-12-07 20:41:46 +0900

    modified update_repos.bat

--- scripts/git/update_repos.bat
+++ scripts/git/update_repos.bat
@@ -1,16 +1,16 @@
 @pushd ..\..\
-@goto label_update
+@call :label_update %1
 @popd
 
 @echo Done.
 @exit /b
 
 :label_update
-@echo function called label_udpate
+@echo function called label_udpate with %1
 @for /D %%d in ("*") do @(
     @echo Current Directory[%%d]
     @cd %%d
     goto label_update
-    @call git pull PJHEngine master
+    @call git pull PJHEngine %1
     @cd ..
 )
