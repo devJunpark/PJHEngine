871d51d21f95bbb8e441eccb8072702bc9a62498
unknown <devjunpark@gmail.com>
2015-12-27 00:17:38 +0900

    modify directory acrc.

--- scripts/_set_variables.bat
+++ scripts/_set_variables.bat
@@ -1,3 +1,11 @@
 @set PROJECT_ROOT_PATH=D:\engine\PJHEngine\PJHEngine
 @set PROJECT_NAME=PJH_Engine
 
+@set TOOL_PATH=%PROJECT_ROOT_PATH%\tool
+@set TOOL_BIN_PATH=%TOOL_PATH%\bin
+
+@set TOOL_CURL_PATH=%TOOL_BIN_PATH%\curl\bin
+@set TOOL_7ZIP_PATH=%TOOL_BIN_PATH%\7zip
+@set TOOL_CMAKE_PATH=%TOOL_BIN_PATH%\cmake-3.4.1-win32-x86\bin
+@set TOOL_NANT_PATH=%TOOL_BIN_PATH%\nant-0.92\bin
+
