ee1fd7b95ff01ecc9c31f33322688333dac70eb4
unknown <devjunpark@gmail.com>
2016-02-15 02:14:00 +0900

    Add prototype for testing of PJHEngine Library.

--- scripts/build/_set_variables.bat
+++ scripts/build/_set_variables.bat
@@ -1,5 +1,7 @@
 @call ..\_set_variables.bat
 
+@set VAR_PROTOTYPE_PROJECT_NAME=Prototype
+
 @set VAR_BIN_PATH=%PROJECT_ROOT_PATH%\bin
 @set VAR_EXT_LINK_PATH=%VAR_BIN_PATH%\dlls
 @set VAR_PACKAGE_PATH=%PROJECT_ROOT_PATH%\packages
