4461a7961de59a6aac9f5ea725191ad07221c726
unknown <devjunpark@gmail.com>
2016-02-17 06:00:16 +0900

    add build output type. and fixed some bugs.

--- scripts/build/cmake_test.bat
+++ scripts/build/cmake_test.bat
@@ -7,6 +7,12 @@
 @pushd %PROJECT_ROOT_PATH%
 
 @set optional=-A x64 -G "Visual Studio 12 2013"
+@set optional=%optional% -Darg.debug:String="true"
+@set optional=%optional% -Darg.locale:String="kor"
+@set optional=%optional% -Darg.rebuild:String="true"
+@set optional=%optional% -Darg.outputtype:String="lib"
+
+@echo %optional%
 
 @if exist %VAR_SCRIPT_BUILD_PATH%\CMakeLists.txt (
     robocopy %VAR_SCRIPT_BUILD_PATH%\ %VAR_PACKAGE_PATH%\%PROJECT_NAME%\ CMakeLists.txt
