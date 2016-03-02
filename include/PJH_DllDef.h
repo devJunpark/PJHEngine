e820b8aac8fe4231bc99c68139e57d0effd34ff3
unknown <devjunpark@gmail.com>
2016-03-02 22:06:55 +0900

    modified build file and cmakelists file. this revision contain files for creating dll and lib solution.

--- include/PJH_DllDef.h
+++ include/PJH_DllDef.h
@@ -1,10 +1,12 @@
 #ifndef __PJH_INCLUDE_PJH_DLLDEF_H__
 #define __PJH_INCLUDE_PJH_DLLDEF_H__
 
-#ifdef __PJH_DLL_API
+#if defined(__PJH_DLL_API) && defined(__PJH_USE_DLL)
 #define PJH_DLL_API __declspec(dllexport)
-#else
+#elif defined(__PJH_USE_DLL)
 #define PJH_DLL_API __declspec(dllimport)
+#else
+#define PJH_DLL_API
 #endif
 
 #endif  __PJH_INCLUDE_PJH_DLLDEF_H__
